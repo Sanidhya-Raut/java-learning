e01c08950f4991b9464f2310617ec7b6a448eabf
Sanidhya <rautsanidhya2@gmail.com>
2026-08-24 23:56:50 +0530

    [Day05] - ExecutorService Day05

---
+++ Day05-ExecutorService/src/main/java/com/sanidhya/day05/Main.java
@@ -0,0 +1,16 @@
+package com.sanidhya.day05;
+
+import com.sanidhya.day05.service.TransactionService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        TransactionService transactionService = new TransactionService();
+        transactionService.work();
+
+    }
+
+
+}
---
+++ Day05-ExecutorService/src/main/java/com/sanidhya/day05/task/TransactionTask.java
@@ -0,0 +1,19 @@
+package com.sanidhya.day05.task;
+
+import com.sanidhya.day05.model.Transaction;
+
+import java.util.concurrent.Callable;
+
+public class TransactionTask implements Callable<String> {
+
+    Transaction transaction;
+
+    public TransactionTask(int id){
+        this.transaction = new Transaction(id);
+    }
+
+    @Override
+    public String call() {
+        return  transaction.getTransactionStatus();
+    }
+}
